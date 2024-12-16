8bb17d5489260218bc55c7700df0a034239df4ec
Benzi Darwin <ssalibenjamin0402@gmail.com>
2024-12-16 21:46:57 +0300

    Fixed minor bug

--- utils/ethereum.ts
+++ utils/ethereum.ts
@@ -2,12 +2,12 @@ import { ethers } from 'ethers';
 
 export async function getEthereumContract(
   contractAddress: string,
-  contractABI: ethers.ContractInterface
+  contractABI: ethers.InterfaceAbi
 ) {
   if (typeof window.ethereum !== 'undefined') {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
     const contract = new ethers.Contract(contractAddress, contractABI, signer);
     return contract;
   }
